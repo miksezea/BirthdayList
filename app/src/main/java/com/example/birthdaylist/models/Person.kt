fbe163bbf2cfa90c03c933157edba5990e66438d
miksezea <mikkel.eilskov@gmail.com>
2023-10-03 13:52:33 +0200

    1. Renamed string values 2. Changed Friends Cards

--- app/src/main/java/com/example/birthdaylist/models/Person.kt
+++ app/src/main/java/com/example/birthdaylist/models/Person.kt
@@ -3,7 +3,7 @@ package com.example.birthdaylist.models
 import com.google.gson.annotations.SerializedName
 
 data class Person(
-    @SerializedName("id") val id: Int = 0,
+    @SerializedName("id") val id: Int,
     @SerializedName("userId") val userId: String = "",
     @SerializedName("name") val name: String = "",
     @SerializedName("birthYear") val birthYear: Int = 0,
@@ -11,8 +11,8 @@ data class Person(
     @SerializedName("birthDayOfMonth") val birthDayOfMonth: Int = 0,
     @SerializedName("pictureUrl") val pictureUrl: String? = null,
     @SerializedName("remarks") val remarks: String? = null,
-    @SerializedName("age") val age: Int = 0) {
+    @SerializedName("age") val age: Int) {
     override fun toString(): String {
-        return "Person($id $name, Birth date: $birthDayOfMonth/$birthMonth - $birthYear Age: $age)"
+        return "Born: $birthDayOfMonth/$birthMonth - $birthYear\nAge: $age"
     }
 }
